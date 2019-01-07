26422b7b6f4044d7adde366609515183f136423c
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-01-06 18:53:02 -0800

    files created and basic links + images added

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+
+
+//psuedo code
+
+// a game with 4 crystals
+//every crystal has a different (random value)
+//generate random every time win or lose
+// add value to score every click 
+// goal is to hit random score
+//random score is randomly generated 19-120
+//random values for crystals between 1-12
+// if crystals add up to randomly generated numbr, increment a win
+// if you go past the goal number you lose
+
+
+$(document).ready(function(){
+
+var winCount = 0;
+var lossCount = 0;
+var 
+
+
+
+
+
+
+
+
+
+
+});
